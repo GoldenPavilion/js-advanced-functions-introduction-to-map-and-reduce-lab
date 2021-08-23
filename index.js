e4af1e73e685984bdec505d37e6f56508059dd22
GoldenPavilion <rcorystine@gmail.com>
2021-08-23 16:41:11 -0400

    completed mapToDouble

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Your code here
 
 function mapToNegativize(sourceArray) {
-    return sourceArray.map(x => x * -1)
+    return sourceArray.map(x => x * -1);
 }
 
 function mapToNoChange(sourceArray) {
-    return sourceArray
+    return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(x => x * 2);
 }
